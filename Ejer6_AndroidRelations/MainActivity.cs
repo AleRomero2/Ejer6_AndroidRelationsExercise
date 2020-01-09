bd84050f923c029b8f3fac29e0d32f0fdeee1466
AleRomero2 <alejandro.romero.rodriguez2@gmail.com>
2020-01-09 19:50:21 +0100

    ejer1 acabao

--- Ejer6_AndroidRelations/MainActivity.cs
+++ Ejer6_AndroidRelations/MainActivity.cs
@@ -6,31 +6,43 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using AlertDialog = Android.App.AlertDialog;
 
 namespace Ejer6_AndroidRelations
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        private Button _button1;
+        private EditText _editText1;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+            AddBindForLayout();
         }
+        private void AddBindForLayout()
+        {
+            _button1 = FindViewById<Button>(Resource.Id.button1);
+            _editText1 = FindViewById<EditText>(Resource.Id.editText1);
 
+            _button1.Click += pruebecita;
+        }
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
             return true;
         }
-
+        private void pruebecita(object sender, EventArgs e)
+        {
+            AlertDialog.Builder ventanaEmergente = new AlertDialog.Builder(this);
+            ventanaEmergente.SetTitle("Hola papi");
+            ventanaEmergente.SetMessage(_editText1.Text.ToString());
+            ventanaEmergente.Show();
+        }
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
             int id = item.ItemId;
